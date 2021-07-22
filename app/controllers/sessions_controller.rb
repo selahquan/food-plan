196eacc615359ecfdd6216d414f662f618746718
Josh <josh.handon@gmail.com>
2021-07-21 21:01:59 -0400

    Login update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,28 @@
 class SessionsController < ApplicationController
+    #before_action :redirect_if_logged_in, only: [:new]
     
+
+    def login
+
+    end
+
+    def create
+        user = User.find_by_username(params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user
+        else
+            flash[:errors] = ["Invalid Username or Password!"]
+            redirect_to login_path
+    end
+
     def logout
         session.clear
         redirect_to new_user_path, notice: "Logged out"
     end
+
+end
+
+private
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  delete "/logout", to: "sessions#logout", as: "logout"
+  get "/login", to: "sessions#login", as: "login"
+  post "/login", to: "sessions#create"
 end
