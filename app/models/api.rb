5bf418a592d927370203e541fb33b929a557234b
Josh <josh.handon@gmail.com>
2021-07-24 10:19:55 -0400

    Api connection

---
+++ app/models/api.rb
@@ -0,0 +1,24 @@
+class Api 
+
+    #self.site = "https://api.spoonacular.com/recipes/complexSearch/?apiKey="
+    #self.site_recipeinfo = "https://api.spoonacular.com/recipes/"
+
+    #self.headers['Authorization'] = 'Token token="ded55717b4eb42968ba0caf2c32416c2"'
+
+    #self.auth_type = :bearer
+    #self.bearer_token = 'ded55717b4eb42968ba0caf2c32416c2'
+
+    def self.complexSearch
+
+        url = URI("https://api.spoonacular.com/recipes/complexSearch/?apiKey=ded55717b4eb42968ba0caf2c32416c2&query=")
+        https = Net::HTTP.new(url.host, url.port)
+        https.use_ssl = true 
+        request = Net::HTTP::Get.new(url)
+
+
+
+        response = https.request(request)
+        puts response.read_body
+
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_190441) do
+ActiveRecord::Schema.define(version: 2021_07_23_190441) do
+
+  create_table "apis", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
