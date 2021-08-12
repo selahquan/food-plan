f86370f02858a2d92bf97efd770bd4236a326912
Josh <josh.handon@gmail.com>
2021-08-12 16:25:12 -0400

    Blended chain

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
     before_action :redirect_if_logged_in, only: [:new]
+    #skip_before_action :verify_authenticity_token
     
 
     def login
 
+    end
+
+    def logout
+        session[:user_id] = nil
+        redirect_to new_user_path, notice: "Logged out"
     end
 
     def create
@@ -14,37 +20,25 @@ class SessionsController < ApplicationController
         else
             flash[:errors] = ["Invalid Username or Password!"]
             redirect_to login_path
+        end
     end
 
-    def logout
-        session[:user_id] = nil
-        redirect_to new_user_path, notice: "Logged out"
-    end
+    
 
     def omniauth
-        user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['onmiauth.auth'][:provider]) do |u|
+        user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['omniauth.auth'][:provider]) do |u|
             u.username = request.env['omniauth.auth'][:info][:first_name]
             u.email = request.env['omniauth.auth'][:info][:email]
             u.password = SecureRandom.hex(15)
-        end
+        end 
         if user.valid?
-            session[:user_id] = user.id
-            redirect_to root_path
+            session[:user_id] = user.id # log them 
+            redirect_to recipes_path
         else
-            redirect_to login_path
+            redirect_to login_path 
         end
-        user = Guest.o_auth_find_info(user_info)
-        user_session_or_redirect(user)
     end
 
-    
-#protected
-    
-#    def auth
-#        request.env['omniauth.auth']["info"]
-#    end
 end
 
 private
-
-end
