46aacad0e1644eaf3a3d34aed30cb5d6d04b7fee
Josh <josh.handon@gmail.com>
2021-08-11 21:03:51 -0400

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,9 +23,16 @@ class SessionsController < ApplicationController
 
     def omniauth
         user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid], provider: request.env['onmiauth.auth'][:provider]) do |u|
-            u.username =
-            u.email =
+            u.username = request.env['omniauth.auth'][:info][:first_name]
+            u.email = request.env['omniauth.auth'][:info][:email]
             u.password = SecureRandom.hex(15)
+        end
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            redirect_to login_path
+        end
         user = Guest.o_auth_find_info(user_info)
         user_session_or_redirect(user)
     end
